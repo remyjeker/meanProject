5c71cc735613c284bfdb2ca79ed29486c31e9a72
Rémy Jeker <remyb.jeker@bnc.ca>
2019-01-29 17:55:04 -0500

    Manage routes / Add ErrorPageComponent

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth-guard.service';
 import { HomeComponent } from '../home/home.component';
+import { ErrorPageComponent } from '../errorPage/errorPage.component';
+
+const errorPageRoute = {
+  path: '**',
+  pathMatch: 'full',
+  component: ErrorPageComponent
+};
 
 const routes: Routes = [{
   path: '',
+  pathMatch: 'full',
+  redirectTo: 'home'
+}, {
+  path: 'home',
   component: HomeComponent
 }, {
   path: 'auth',
@@ -12,7 +23,9 @@ const routes: Routes = [{
 }, {
   path: 'admin',
   loadChildren: 'app/admin/admin.module#AdminModule'
-}];
+},
+  errorPageRoute
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
